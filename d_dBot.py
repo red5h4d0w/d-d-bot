d6311cf25e7efc866fbe9cea54a85d654d1fec44
Ismaël Bussière <45214944+red5h4d0w@users.noreply.github.com>
2019-06-11 14:17:37 -0400

    Modified for Discord.py v1 (Rewrite)

--- d_dBot.py
+++ d_dBot.py
@@ -1,20 +1,33 @@
 import random
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
+import sqlite3
 
 bot_prefix = "d."
 bot = commands.Bot(command_prefix=bot_prefix)
 
+def id_already_exists(id):
+	c.execute("SELECT FROM discord WHERE ID = ?",id)
+	if c.fetchone() == None:
+		return False
+	return True
+
+def read_token():
+	'''Reads the token located in a text file'''
+	with open("token.txt", "r") as f:
+		lines = f.readlines()
+		return lines[0].strip()
+
 def char_create():
 	X = []
-	for i in range(6):
+	for _ in range(6):
 		c = []
-		for t in range(4):
+		for _ in range(4):
 			c.append(random.randint(1,6))
 		c.remove(min(c))
 		X.append(c[0]+c[1]+c[2])
 	return X
+
 @bot.event
 async def on_ready():
     print("Bot en ligne")
@@ -23,15 +36,36 @@ async def on_ready():
 
 @bot.command(pass_context=True)
 async def commandes(ctx):
-    await bot.say(" Les commandes sont : \n\t\t\t\t\t\t\t\t\t\td.commandes\n\t\t\t\t\t\t\t\t\t\td.d20 : lance un dé 20 ")
+    await ctx.send(" Les commandes sont : \n\t\t\t\t\t\t\t\t\t\td.commandes\n\t\t\t\t\t\t\t\t\t\td.d20 : lance un dé 20 ")
 
 @bot.command(pass_context=True)
-async def d20(ctx):
-    await bot.say("tu as roulé : " + str(random.randint(1,20)))
+async def roule(ctx, dés: str):
+	"""Roule des dés en format dnd"""
+	try:
+		lancers, faces = map(int, dés.split('d'))
+	except Exception:
+		await ctx.send("Écrit sous la forme NdN stp")
+		return
+	résultats = []
+	for _ in range(0, lancers):
+		résultats.append(random.randint(1,faces))
+	await ctx.send("tu as roulé : " + str(sum(résultats)))
+	
+@bot.event
+async def on_message(message):
+	id = message.author.id
+	name = message.author.name
+	if not id_already_exists(id):
+		c.execute("INSERT INTO discord VALUES(:id, :XP, :name )", {"id": id, "XP": 0, "name": name})
+	c.execute("UPDATE discord SET XP = XP + 1 WHERE id=?", id)
+	conn.commit()
+	await bot.process_commands(message)
 	
 @bot.command(pass_context=True)
 async def perso(ctx):
 	X=char_create()
-	await bot.say(str(X[0])+"\n"+str(X[1])+"\n"+str(X[2])+"\n"+str(X[3])+"\n"+str(X[4])+"\n"+str(X[5]))	
-
-bot.run("NDU0NjI5MzQ1Mjk4NzQzMjk2.DfzrCQ.QW_EJt_bjBWQfywSzkMmcOGuk3A")
+	await ctx.send(str(X[0])+"\n"+str(X[1])+"\n"+str(X[2])+"\n"+str(X[3])+"\n"+str(X[4])+"\n"+str(X[5]))	
+if __name__ == "__main__":
+	conn = sqlite3.connect("d-d-bot.db")
+	c = conn.cusror()
+	bot.run(read_token())
